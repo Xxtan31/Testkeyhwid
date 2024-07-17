8c66413870631fa2cf3b31c799d8c7a66bcce14f
Xxtan31 <102824999+Xxtan31@users.noreply.github.com>
2024-07-18 01:36:56 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,38 @@
-if (navigator.userAgent.includes("Brave") || navigator.userAgent.includes("Kiwi")) {
-    window.location.href = "error.html";
+function proceed() {
+    const hwid = new URLSearchParams(window.location.search).get('hwid');
+    if (hwid) {
+        const nextUrl = document.body.dataset.nextUrl;
+        window.location.href = `${nextUrl}?hwid=${hwid}`;
+    } else {
+        console.error('HWID not found in URL');
+    }
 }
+
+function isKeyValid() {
+    const now = new Date().getTime();
+    const expiration = localStorage.getItem('expiration');
+    return expiration && now < expiration;
+}
+
+window.onload = function() {
+    if (isKeyValid()) {
+        const hwid = new URLSearchParams(window.location.search).get('hwid');
+        window.location.href = `get-key.html?hwid=${hwid}`;
+    }
+};
+
+async function detectBrave() {
+    const isBrave = (window.navigator.brave && await window.navigator.brave.isBrave()) || false;
+    return isBrave;
+}
+
+function detectKiwi() {
+    const userAgent = navigator.userAgent || navigator.vendor || window.opera;
+    return userAgent.includes("Kiwi");
+}
+
+(async function() {
+    if (await detectBrave() || detectKiwi()) {
+        window.location.href = "error.html";
+    }
+})();
