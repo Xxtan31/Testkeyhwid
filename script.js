645a3202fdc63ef7f6cc6050888ce81196287dcd
Xxtan31 <102824999+Xxtan31@users.noreply.github.com>
2024-07-18 00:54:23 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,3 @@
-.then(data => {
-    // Anahtarın döndüğünü kontrol et
-    const generatedKey = data.message;
-    if (generatedKey.startsWith("ATA-")) {
-        window.location.href = `key_display.html?key=${generatedKey}`;
-    } else {
-        console.error('Geçersiz anahtar:', generatedKey);
-    }
-})
-
 if (navigator.userAgent.includes("Brave") || navigator.userAgent.includes("Kiwi")) {
     window.location.href = "error.html";
 }
